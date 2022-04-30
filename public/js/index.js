fe780eeaa97c20d1f37985dfc9051d59b4a2e6a6
NicholasCH <nicholasch96@gmail.com>
2022-04-30 01:39:01 -0500

    refactor order pwindb

--- public/js/index.js
+++ public/js/index.js
@@ -29,10 +29,10 @@ function populateTable() {
   transactions.forEach(transaction => {
     // create and populate a table row
     let tr = document.createElement("tr");
-    tr.innerHTML = `
-      <td>${transaction.name}</td>
-      <td>${transaction.value}</td>
-    `;
+      tr.innerHTML = `
+        <td>${transaction.name}</td>
+        <td>${transaction.value}</td>
+      `;
 
     tbody.appendChild(tr);
   });
--- public/js/pwindb.js
+++ public/js/pwindb.js
@@ -1,63 +1,70 @@
-// const { response } = require("express");
-
 let db
 const request = indexedDB.open('Pwudget', 1);
 
-request.onupgradeneeded = function(evt) {
-    db = evt.target.result;
-    db.createObjectStore('new_pwudge', { autoIncrement: true });
-  };
+
+function uploadPwudge() {
+    const transaction = db.transaction(['new_pwudge'], 'readwrite');
+    const pwudgeObjStore = transaction.objectStore('new_pwudge');
+    const getAll = pwudgeObjStore.getAll();
+    getAll.onsuccess = ()=> {
+
+        if (getAll.result.length > 0) {
+        fetch('/api/transaction', {
+            method: 'POST',
+            body: JSON.stringify(getAll.result),
+            headers: {
+                Accept: 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((response) => response.json())
+            .then((mess) => {
+            if (mess.message) {
+                throw new Error(mess);
+            }
+
+            const transaction = db.transaction(['new_pwudge'], 'readwrite');
+            const pwudgeObjStore = transaction.objectStore('new_pwudge');
+                pwudgeObjStore.clear();
+                alert('Transactions Uploaded!')
+                
+                    location.reload();
+            })
+            .catch(err => {
+            console.log(err);
+            });
+        }
+        };
+}
+function saveRecord(record) {
+    const transaction = db.transaction(['new_pwudge'], 'readwrite');
+    const pwudgeObjStore = transaction.objectStore('new_pwudge');
+    pwudgeObjStore.add(record);
+}
+
 
 request.onsuccess = function(evt) {
     db = evt.target.result;
     if (navigator.onLine) {
-    uploadPwudge();
-}
-};
-
-request.onerror = function(evt) {
-console.log(evt.target.errorCode);
-};
-
-
-function saveRecord(record) {
-    const transaction = db.transaction(['new_pwudge'], 'readwrite');
-    const pwudgeObjStore = transaction.objectStore('new_pwudge');
-pwudgeObjStore.add(record);
-}
-
-function uploadPwudge() {
-const transaction = db.transaction(['new_pwudge'], 'readwrite');
-const pwudgeObjStore = transaction.objectStore('new_pwudge');
-const getAll = pwudgeObjStore.getAll();
-getAll.onsuccess = function() {
-
-    if (getAll.result.length > 0) {
-    fetch('/api/transaction', {
-        method: 'POST',
-        body: JSON.stringify(getAll.result),
-        headers: {
-            Accept: 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-        }
-    })
-        .then((response) => response.json())
-        .then((mess) => {
-        if (mess.message) {
-            throw new Error(mess);
-        }
-
-        const transaction = db.transaction(['new_pwudge'], 'readwrite');
-        const pwudgeObjStore = transaction.objectStore('new_pwudge');
-            pwudgeObjStore.clear();
-            location.reload();
-        })
-        .catch(err => {
-        console.log(err);
-        });
+        uploadPwudge();
     }
 };
-}
+
+request.onupgradeneeded = function(evt) {
+    db = evt.target.result;
+    db.createObjectStore('new_pwudge', { 
+        autoIncrement: true 
+    });
+};
+
+
+request.onerror = function(evt) {
+    console.log(evt.target.errorCode);
+};
+
+
+
+
 
 // listen for app coming back online
 window.addEventListener("online", uploadPwudge);
